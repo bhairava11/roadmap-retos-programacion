1c3188555a76ba63d686248f0bd530622da88564
bhairava11 <bhairava1@hotmail.com>
2025-01-30 20:49:38 -0600

    Create bhairava11.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/bhairava11.py
@@ -0,0 +1,47 @@
+# https://python.org 
+
+# esto en python es un comentario en una sola linea
+
+"""
+tres dobles comillas para
+abrir un comentario
+en varias lineas
+"""
+
+'''
+tres  comillas simples
+abren tambien un comentario
+en varias lineas 
+y se cierra igualmente con tres
+comillas simples
+'''
+
+My_variable = "mi tarea"
+My_variable = "Nuevo valor de mi tarea"
+
+"""
+En python no existen 
+constantes, la unica 
+constante son las variables
+"""
+
+MY_CONSTANTE = "esta es mi constante" # por conveccion 
+MY_CONSTANTE = " aqui puedo darle el valor que quiera"
+
+my_int =  8
+my_float = 2.6
+my_booleano = True
+my_booleano = False
+my_string = "Mi variable tipo cadena texto"
+my_other_string = 'Mi otra variable tipo cadena de texto con comillas simples'
+
+print ("¡Hola, Python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_booleano))
+print(type(my_booleano))
+print(type(my_string))
+
+
+
